66c204d89fd7d08c0c3187d542d455a3c9a7a916
drmmx <m.daglarov@gmail.com>
2018-03-26 14:45:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mdagl/bluechat/RegisterActivity.java
@@ -0,0 +1,86 @@
+package com.example.mdagl.bluechat;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "My tag";
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateAccountBtn;
+
+    private FirebaseAuth mAuth;
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDisplayName = (TextInputLayout) findViewById(R.id.reg_name);
+        mEmail = (TextInputLayout) findViewById(R.id.reg_e_mail);
+        mPassword = (TextInputLayout) findViewById(R.id.reg_pass);
+        mCreateAccountBtn = (Button) findViewById(R.id.reg_btn);
+
+        mCreateAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String displayNameText = mDisplayName.getEditText().getText().toString();
+                String emailText = mEmail.getEditText().getText().toString();
+                String passwordText = mPassword.getEditText().getText().toString();
+
+                if (!TextUtils.isEmpty(displayNameText) || !TextUtils.isEmpty(emailText) || !TextUtils.isEmpty(passwordText)) {
+                    registerUser(displayNameText, emailText, passwordText);
+                }
+
+            }
+        });
+    }
+
+    private void registerUser(String displayNameText, String emailText, String passwordText) {
+
+        mAuth.createUserWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Cannot register user. Please check the form and try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+}
---
+++ app/src/main/java/com/example/mdagl/bluechat/StartActivity.java
@@ -0,0 +1,38 @@
+package com.example.mdagl.bluechat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button mRegBtn;
+    private Button mSignInBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        mRegBtn = (Button) findViewById(R.id.start_reg_button);
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mSignInBtn = (Button) findViewById(R.id.start_login_button);
+        mSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
